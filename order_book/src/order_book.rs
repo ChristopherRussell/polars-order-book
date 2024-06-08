d7147528fea43fc4ea56c5f1cba552b53ca69e6b
ChristopherRussell <19709603+ChristopherRussell@users.noreply.github.com>
2024-06-08 09:30:23 +0100

    order_book: add modify tests

--- order_book/src/order_book.rs
+++ order_book/src/order_book.rs
@@ -12,7 +12,7 @@ pub struct OrderBook<Price, Qty> {
 }
 
 impl<Price: Copy + Debug + Display + Hash + Ord, Qty: Copy + Debug + Display + Num + Ord> Default
-for OrderBook<Price, Qty>
+    for OrderBook<Price, Qty>
 {
     fn default() -> Self {
         Self::new()
@@ -20,7 +20,7 @@ for OrderBook<Price, Qty>
 }
 
 impl<Price: Copy + Debug + Display + Hash + Ord, Qty: Copy + Debug + Display + Num + Ord>
-OrderBook<Price, Qty>
+    OrderBook<Price, Qty>
 {
     pub fn new() -> Self {
         OrderBook {
@@ -101,5 +101,29 @@ mod tests {
         assert_eq!(order_book.book_side(true).get_level(100).unwrap().qty, 5);
         order_book.delete_qty(true, 100, 5);
         assert!(order_book.book_side(true).get_level(100).is_none());
+    }
+
+    #[test]
+    fn test_modify_qty() {
+        for is_bid in vec![true, false] {
+            let mut order_book = OrderBook::default();
+            order_book.add_qty(is_bid, 100, 10);
+            assert_eq!(order_book.book_side(is_bid).get_level(100).unwrap().qty, 10);
+            order_book.modify_qty(is_bid, 100, 10, 100, 20);
+            assert_eq!(order_book.book_side(is_bid).get_level(100).unwrap().qty, 20);
+        }
+    }
+
+    #[test]
+    fn test_modify_price() {
+        for is_bid in vec![true, false] {
+            let mut order_book = OrderBook::default();
+            order_book.add_qty(is_bid, 1, 1);
+            assert_eq!(order_book.book_side(is_bid).get_level(1).unwrap().qty, 1);
+            order_book.modify_qty(is_bid, 1, 1, 2, 2);
+            assert_eq!(order_book.book_side(is_bid).get_level(2).unwrap().qty, 2);
+            order_book.modify_qty(is_bid, 2, 2, 1, 1);
+            assert_eq!(order_book.book_side(is_bid).get_level(1).unwrap().qty, 1);
+        }
     }
 }
