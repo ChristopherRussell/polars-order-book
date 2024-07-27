8ed2e71c00ee186e01a03fa42919d4dd94e2a795
ChristopherRussell <19709603+ChristopherRussell@users.noreply.github.com>
2024-07-27 01:54:47 +0100

    re-add basic (one level) tracking code

--- order_book/src/lib.rs
+++ order_book/src/lib.rs
@@ -4,5 +4,7 @@ pub mod price_level;
 
 pub mod book_side_ops;
 pub mod book_side_tracked;
+pub mod book_side_tracked_basic;
 pub mod order_book_tracked;
+pub mod order_book_tracked_basic;
 pub mod top_n_levels;
